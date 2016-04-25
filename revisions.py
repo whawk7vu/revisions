30b2b452b51623ec4a19fdcbf957e77a66dd013e
whawk7vu <williamhawk@gmail.com>
2016-04-25 12:27:59 -0400

    Working

--- revisions.py
+++ revisions.py
@@ -4,89 +4,68 @@ Created on Fri Mar 25 13:34:41 2016
 
 @author: Swan
 """
-#Hello?
-#import a bunch of stuff
 import pandas as pd
 import requests
 import datetime
 
-# table with basic information https://research.stlouisfed.org/fred2/release/tables?rid=53&eid=13562
-
-#percent_change_list
-#GDP: A191RL1Q225SBEA
-    #Goods: https://research.stlouisfed.org/fred2/series/A353RY2Q224SBEA
-    #Services: https://research.stlouisfed.org/fred2/series/DSERRY2Q224SBEA
-#Investment: https://research.stlouisfed.org/fred2/series/A006RY2Q224SBEA
-    #Nonresidential: https://research.stlouisfed.org/fred2/series/A008RY2Q224SBEA
-    #Residential: https://research.stlouisfed.org/fred2/series/A011RY2Q224SBEA
-    #Inventories: https://research.stlouisfed.org/fred2/series/A014RY2Q224SBEA
-#Net exports: https://research.stlouisfed.org/fred2/series/A019RY2Q224SBEA
-    #Exports: https://research.stlouisfed.org/fred2/series/A020RY2Q224SBEA
-        #Goods: https://research.stlouisfed.org/fred2/series/A253RY2Q224SBEA
-        #Services: https://research.stlouisfed.org/fred2/series/A646RY2Q224SBEA
-    #Imports: https://research.stlouisfed.org/fred2/series/A021RY2Q224SBEA
-        #Goods: https://research.stlouisfed.org/fred2/series/A255RY2Q224SBEA
-        #Services: https://research.stlouisfed.org/fred2/series/A656RY2Q224SBEA
-#Government: https://research.stlouisfed.org/fred2/series/A822RY2Q224SBEA
-    #Federal: https://research.stlouisfed.org/fred2/series/A823RY2Q224SBEA
-        #Defense: https://research.stlouisfed.org/fred2/series/A824RY2Q224SBEA
-        #Nondefense: https://research.stlouisfed.org/fred2/series/A825RY2Q224SBEA
-    #State and local: https://research.stlouisfed.org/fred2/series/A829RY2Q224SBEA
-
-
+writer = pd.ExcelWriter('output.xlsx')
 
 with open('api.txt', 'r') as api:
     api=api.read()
 
 percent_change_list = ["A353RY2Q224SBEA", "DSERRY2Q224SBEA", "A006RY2Q224SBEA", "A008RY2Q224SBEA",	"A011RY2Q224SBEA",	"A014RY2Q224SBEA",	"A019RY2Q224SBEA",	"A020RY2Q224SBEA",	"A253RY2Q224SBEA",	"A646RY2Q224SBEA",	"A021RY2Q224SBEA",	"A255RY2Q224SBEA",	"A656RY2Q224SBEA",	"A822RY2Q224SBEA",	"A823RY2Q224SBEA",	"A824RY2Q224SBEA",	"A825RY2Q224SBEA",	"A829RY2Q224SBEA"]
-#series_list = ['GDPC1', 'PCECC96', 'GPDIC1', 'NETEXC', 'GCEC96']
-series_id = 'A191RL1Q225SBEA'
+indicator = ''
 
-r = requests.get('https://api.stlouisfed.org/fred/series/observations?series_id=' + series_id + '&realtime_start=1776-07-04&api_key=' + api + '&file_type=json')
+mylist = percent_change_list
 
-series = r.json()
+for element in mylist:
 
-obs = series['observations']
+    r = requests.get('https://api.stlouisfed.org/fred/series/observations?series_id=' + element + '&realtime_start=1776-07-04&api_key=' + api + '&file_type=json')
+    
+    series = r.json()
+    
+    obs = series['observations']
+    
+    for obj in obs:
+        obj['new_date'] = datetime.datetime.strptime(obj['date'],'%Y-%m-%d').date()
+        obj['start'] = datetime.datetime.strptime(obj['realtime_start'],'%Y-%m-%d').date()
+        obj['end'] = datetime.datetime.strptime(obj['realtime_end'],'%Y-%m-%d').date()
+        if obj['new_date'].month == 1:
+            obj['date_label'] = str(obj['new_date'].year) + ' Q1'
+        elif  obj['new_date'].month == 4:
+            obj['date_label'] = str(obj['new_date'].year) + ' Q2'
+        elif  obj['new_date'].month == 7:
+            obj['date_label'] = str(obj['new_date'].year) + ' Q3'
+        elif  obj['new_date'].month == 10:
+            obj['date_label'] = str(obj['new_date'].year) + ' Q4'
+        else:
+            obj['date_label'] = 'NaN'
+         
+        ###I think this should be the function, but I am having some trouble        
+        if (obj['new_date'] + datetime.timedelta(days=91) < obj['start'] and
+            obj['new_date'] + datetime.timedelta(days=121) > obj['start']):
+                obj['label'] = 'First'
+        elif (obj['new_date'] + datetime.timedelta(days=121) < obj['start'] and
+              obj['new_date'] + datetime.timedelta(days=152) > obj['start']):
+                  obj['label'] = 'Second'
+        elif (obj['new_date'] + datetime.timedelta(days=152) < obj['start'] and
+              obj['new_date'] + datetime.timedelta(days=182) > obj['start']):
+                  obj['label'] = 'Third'
+        else: obj['label'] = 'NaN'
+          
+    vals_by_date={}
+    series_change={}
+    
+    # read in observations
+    for item in obs:
+        if not item['date_label'] in vals_by_date:
+            vals_by_date[item['date_label']] = {item['label']:float(item['value'])}
+        else:
+            vals_by_date[item['date_label']].update({item['label']:float(item['value'])})
+        if item['realtime_end'] == '9999-12-31':
+            vals_by_date[item['date_label']].update({'current':float(item['value'])})
+    
+    r = pd.DataFrame.from_dict(vals_by_date, orient='index', dtype=None)
+    r.to_excel(writer, element)
 
-for obj in obs:
-    obj['new_date'] = datetime.datetime.strptime(obj['date'],'%Y-%m-%d').date()
-    obj['start'] = datetime.datetime.strptime(obj['realtime_start'],'%Y-%m-%d').date()
-    obj['end'] = datetime.datetime.strptime(obj['realtime_end'],'%Y-%m-%d').date()
-    if obj['new_date'].month == 1:
-        obj['date_label'] = str(obj['new_date'].year) + ' Q1'
-    elif  obj['new_date'].month == 4:
-        obj['date_label'] = str(obj['new_date'].year) + ' Q2'
-    elif  obj['new_date'].month == 7:
-        obj['date_label'] = str(obj['new_date'].year) + ' Q3'
-    elif  obj['new_date'].month == 10:
-        obj['date_label'] = str(obj['new_date'].year) + ' Q4'
-    else:
-        obj['date_label'] = 'NaN'
-     
-    ###I think this should be the function, but I am having some trouble        
-    if (obj['new_date'] + datetime.timedelta(days=91) < obj['start'] and
-        obj['new_date'] + datetime.timedelta(days=121) > obj['start']):
-            obj['label'] = 'First'
-    elif (obj['new_date'] + datetime.timedelta(days=121) < obj['start'] and
-          obj['new_date'] + datetime.timedelta(days=152) > obj['start']):
-              obj['label'] = 'Second'
-    elif (obj['new_date'] + datetime.timedelta(days=152) < obj['start'] and
-          obj['new_date'] + datetime.timedelta(days=182) > obj['start']):
-              obj['label'] = 'Third'
-    else: obj['label'] = 'NaN'
-      
-vals_by_date={}
-series_change={}
-
-# read in observations
-for item in obs:
-    if not item['date_label'] in vals_by_date:
-        vals_by_date[item['date_label']] = {item['label']:float(item['value'])}
-    else:
-        vals_by_date[item['date_label']].update({item['label']:float(item['value'])})
-    if item['realtime_end'] == '9999-12-31':
-        vals_by_date[item['date_label']].update({'current':float(item['value'])})
-
-vals_by_date
-
-
+writer.save()

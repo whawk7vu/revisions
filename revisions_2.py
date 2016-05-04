86b1220a250842a66486ccef4b5dae15e77c484d
whawk7vu <williamhawk@gmail.com>
2016-05-04 11:20:32 -0400

    Improved comments

--- revisions_2.py
+++ revisions_2.py
@@ -12,26 +12,31 @@ import datetime
 ### Step 1.0 Create URLS dataframe 
 ###
 
+#clean  up urls
 urls = pd.read_csv('url.csv', index_col = 0)
-
 urls.x = urls.x.str.upper()
-
 urls.x = urls.x.str.replace('HTTP://WWW.BEA.GOV/HISTDATA/RELEASES/GDP_AND_PI/','')
-
 urls.x = urls.x.str.replace('_','/')
 
+#change to pandas dataframe
 urls = pd.DataFrame(urls.x.str.split('/').tolist())
 
+#rename columns
 urls.columns = ['year', 'quarter', 'est', 'date', 'section', 'xls']
 
+#Change date to datetime.date() object
 urls['date_pub'] = urls['date'].apply(lambda x: datetime.datetime.strptime(x,'%B-%d-%Y').date())
 
+#Change Estimate names
 urls.est.replace({'PRELIMINARY': 'SECOND', 'FINAL': 'THIRD'}, regex=True, inplace=True)
 
+#sort by date
 urls.sort_values('date_pub', inplace=True)
 
+#create date variable year_Q
 urls['date'] = urls['year'] + '_' + urls['quarter']
 
+#keep relevent columns
 urls = urls[['date','est', 'date_pub']]
 #output urls to csv
 #urls.to_csv('urls.csv')
@@ -43,18 +48,20 @@ urls = urls[['date','est', 'date_pub']]
 x=0
 #Range should be 1 to 150. using 10 just to test.
 for x in range(1, 10):
+    #read in xls files
     xls_file = pd.ExcelFile('histData' + str(x) + '.xls')
     
     if '10105 Qtr' in xls_file.sheet_names:
+        
+        #This section is simply to get the date_pub variable to match with date_pub from urls
         hist_date = xls_file.parse(sheetname = '10105 Qtr', header=None)
         my_list = hist_date[0].astype(str)
         matching = [s for s in my_list if "Data published" in s]
         matching = [matching.replace("Data published","") for matching in matching]
-        
         #change date into datime.date() format
         date_pub = datetime.datetime.strptime(matching[0].strip(' '), '%B %d, %Y').date()
         
-        #parse each xls file
+        #Get the actual data values by parsing each xls file
         hist_file = xls_file.parse(sheetname = '10105 Qtr', skiprows=7, header=None)
         
         #Change rows into column names
@@ -74,7 +81,7 @@ for x in range(1, 10):
         #test = hist_file[list(hist_file.columns[:2]) + list(hist_file.columns[:-2])].copy()
 
         #Save these files to show what I want:        
-        hist_file.to_csv('mock'+str(x)+'.csv')
+       #hist_file.to_csv('mock'+str(x)+'.csv')
 
 
 ###        

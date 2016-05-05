9ed0093095c33f0bd20ec8dfdd80f299db4d14b8
whawk7vu <williamhawk@gmail.com>
2016-05-05 00:18:54 -0400

    Working copy

--- revisions_2.py
+++ revisions_2.py
@@ -38,7 +38,13 @@ urls['date'] = urls['year'] + '_' + urls['quarter']
 
 #keep relevent columns
 urls = urls[['date','est', 'date_pub']]
+
+#keep only files after 2004_Q1
+urls = urls[urls['date_pub'] >= pd.datetime(2004, 4, 1).date()]
+
+
 #output urls to csv
+
 #urls.to_csv('urls.csv')
 
 ###
@@ -46,8 +52,10 @@ urls = urls[['date','est', 'date_pub']]
 ###
 
 x=0
+hist_file_all = pd.DataFrame()
+long_file = pd.DataFrame()
 #Range should be 1 to 150. using 10 just to test.
-for x in range(1, 10):
+for x in range(1, 149):
     #read in xls files
     xls_file = pd.ExcelFile('histData' + str(x) + '.xls')
     
@@ -68,9 +76,10 @@ for x in range(1, 10):
         hist_col = hist_file[:2].transpose()
         hist_col["period"] = hist_col[0].apply(str).str[:4] + '_Q' + hist_col[1].apply(str).str[:1]
         col_names = hist_col['period'].tolist()
-        col_names[0] = 'Line'
-        col_names[1] = 'Description'
-        col_names[2] = 'Code'
+        col_names[0] = 'line'
+        col_names[1] = 'description'
+        col_names[2] = 'code'
+        col_names[-1] = 'value'
         hist_file.columns = col_names
         
         #drop NAs
@@ -79,92 +88,32 @@ for x in range(1, 10):
         #add date_pub to the files
         hist_file['date_pub'] = date_pub
         #test = hist_file[list(hist_file.columns[:2]) + list(hist_file.columns[:-2])].copy()
-
+        
+        
+        hist_file = hist_file.ix[:,[0,1,2,-1,-2]]
         #Save these files to show what I want:        
         #hist_file.to_csv('mock'+str(x)+'.csv')
-
-
-###        
-#This is where I can't figure it out
-###
+        hist_file_all = pd.concat([hist_file_all, hist_file])
         
-test = hist_file[hist_file.columns[0:3], hist_file.columns[4]] 
+        if x==1:
+            codes = hist_file['code']
+            urls_all = urls
+            for item in codes:
+                urls_all['code'] = item
+                long_file = pd.concat([long_file, urls_all])
 
-newdf = df[df.columns[2:4]]
+hist_file_all.sort_values(by=['date_pub', 'line'], inplace=True)
+hist_file_all.ix[hist_file_all['date_pub']==pd.datetime(2007, 1, 31).date(), 'date_pub'] = pd.datetime(2007, 1, 27).date()
+hist_file_all.ix[hist_file_all['date_pub']==pd.datetime(2007, 3, 29).date(), 'date_pub'] = pd.datetime(2007, 3, 30).date()
+final_data = pd.merge(long_file, hist_file_all, how='left', on=['date_pub', 'code'])
 
-len(hist_file.index)
-        result =  pd.DataFrame()
-                
-        for hist_row in hist_file.iterrows():
-            for urls_row in urls.iterrows():
-                result['Code'] = hist_row['Code']
-                result['date'] = urls_row['date']
-                result['est'] = urls_row['est']
-                result['date_pub'] = urls_row['date_pub']
-                result['value'] = urls_row['value']
-            
-        for hist_row in hist_file:
-            for urls_row in urls:
-                if hist_row['date_pub'] == urls_row['date_pub']:
-                    print("True")
-                else:
-                    print("False")
+
 '''                   
 #Use this when you get the code going                    
     elif '101 Qtr' in xls_file.sheet_names:
         xls_file = xls_file.parse(sheetname = '101 Qtr', header=None)
         hist_file = xls_file.parse(sheetname = '101 Qtr', skiprows=7, skip_footer=0)
 '''                   
-                    
-#Testing ground
-
-hist_test = hist_file  
-urls_test = urls     
-final_test = urls  
-
-for index1, row1 in hist_test.iterrows():
-    
-    for index2, row2 in urls_test.iterrows():
-        
-        if row1['date_pub'] == row2['date_pub']:
-            final_test['value'] = hist_test.ix[:,-2]
-            print("True")
-        else:
-            print("False")
         
 
-
-
-for r in hist_test:
-    print(hist_test['Code'][0])
-
-if hist_test['date_pub'] == urls_test['date_pub']:
-    print("True")
-else:
-    print("False")
-            
-                    
-xls_file = pd.ExcelFile('histData2.xls')
-
-hist_date = xls_file.parse(sheetname = '10105 Qtr', header=None)
-
-my_list = hist_date[0].astype(str)
-matching = [s for s in my_list if "Data published" in s]
-matching = [matching.replace("Data published","") for matching in matching]
-date_pub = datetime.datetime.strptime(matching[0].strip(' '), '%B %d, %Y').date()
-
-hist_file = xls_file.parse(sheetname = '10105 Qtr', skiprows=7, header=None)
-hist_col = hist_file[:2].transpose()
-
-hist_col["period"] = hist_col[0].apply(str).str[:4] + '_Q' + hist_col[1].apply(str).str[:1]
-
-col_names = hist_col['period'].tolist()
-col_names[0] = 'Line'
-col_names[1] = 'Description'
-col_names[2] = 'Code'
-
-hist_file.columns = col_names
-hist_file.dropna(inplace=True)
-hist_file['date_pub'] = date_pub
-#Save these files to show what I want:        
-hist_file.to_csv('mock2.csv')
+           

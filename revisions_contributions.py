9b1ff3bccd4811579f06d70b1c2e931e288c0dbe
Hawk <WHawk@doc.gov>
2016-06-06 10:38:10 -0400

    embeded files in html

--- revisions_contributions.py
+++ revisions_contributions.py
@@ -8,18 +8,8 @@ Created on Thu May  5 09:23:34 2016
 import bokeh
 import pandas as pd
 import datetime
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-
-from plotly import __version__
-from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
-
-from plotly.offline import plot
-from plotly.graph_objs import Scatter
 
 from bokeh.plotting import figure, output_file, show, save
-
-plt.style.use('ggplot')
 
 ###
 ### Step 1.0 Create URLS dataframe 
@@ -347,6 +337,99 @@ columns = [
 data_table = DataTable(source=source, columns=columns, width=2000, height=1000)
 
 show(vform(data_table))
+
+
+
+
+
+#########################################################################################################
+
+
+
+import random
+
+from jinja2 import Template
+
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.browser import view
+
+########## BUILD FIGURES ################
+
+PLOT_OPTIONS = dict(plot_width=800, plot_height=300)
+SCATTER_OPTIONS = dict(size=12, alpha=0.5)
+
+data = lambda: [random.choice([i for i in range(100)]) for r in range(10)]
+
+source = ColumnDataSource(main_table)
+columns = [
+        TableColumn(field=c, title=c) for c in main_table.columns 
+    ]
+data_table = DataTable(source=source, columns=columns, width=2000, height=1000)
+
+
+red = figure(width=800, height=400, title=temp_graph['description'].iloc[0], x_axis_type="datetime", y_range=(temp_graph['current'].min() - abs(temp_graph['current'].min()*.1), temp_graph['current'].max() + abs(temp_graph['current'].max()*.1)), outline_line_color = None)
+red.xgrid.grid_line_color = None
+red.ygrid.grid_line_color = None
+red.yaxis.minor_tick_line_color = None
+red.xaxis.minor_tick_line_color = None
+red.quad(top=temp_graph['current'], bottom=0, left=temp_graph['date_t'][:-1] + pd.DateOffset(10) , right=temp_graph['date_t'][1:] - pd.DateOffset(10)) 
+red.line(temp_graph['date_t'], temp_graph['abs_two_year'], color='red', line_width=3, legend="Two-year absolute revision")
+red.legend.location = "bottom_left"
+
+
+green = data_table
+
+
+########## RENDER PLOTS ################
+
+# Define our html template for out plots
+template = Template('''<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Responsive plots</title>
+        {{ js_resources }}
+        {{ css_resources }}
+    </head>
+    <body>
+    <h2>Resize the window to see some plots resizing</h2>
+    <h3>Red - pan tool, responsive</h3>
+    {{ plot_div.red }}
+    <h3>Green - pan and resize tools, responsive (maintains new aspect ratio)</h3>
+    {{ plot_div.green }}
+    <h3>Blue - pan tool, not responsive</h3>
+    {{ plot_div.blue }}
+    {{ plot_script }}
+    </body>
+</html>
+''')
+
+resources = INLINE
+
+js_resources = resources.render_js()
+css_resources = resources.render_css()
+
+script, div = components({'red': red, 'blue': blue, 'green': green})
+
+html = template.render(js_resources=js_resources,
+                       css_resources=css_resources,
+                       plot_script=script,
+                       plot_div=div)
+
+filename = 'embed_multiple_responsive.html'
+
+with open(filename, 'w') as f:
+    f.write(html)
+
+view(filename)
+
+
+###############################
+
+
+
 
 
            
